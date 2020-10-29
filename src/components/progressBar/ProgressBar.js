01e54bfd4595006abe433fffc0dce48c019b8466
rasdinho <ras.dinho7@gmail.com>
2020-10-29 17:12:11 -0400

    progress-bar

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-
-// import './Styles.scss';
-
-// const ProgressBar = ({id, totalQuestions}) => {
-
-//   return(
-//     <div className="progressBar">
-//       <div className="progressBar--fill" style={{ width : (id + 1) / (totalQuestions + 1) * 100 + "%"}} />
-//       <div className="progressBar--background"></div>
-//     </div>
-//   )
-// }
-
-// export default ProgressBar;
-
-
-
-
-
 
 
 import React from 'react';
